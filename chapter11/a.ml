97363d86d5c63127292f7085490e7d005a26623a
Tatsuya Sato <satoryu.1981@gmail.com>
2021-04-02 15:29:59 +0900

    Complete Chapter 11's exercises

---
+++ chapter11/a.ml
@@ -0,0 +1,13 @@
+(* 問題 11.2 *)
+(* 目的: 漸化式の第n項を求める *)
+(* a: int -> int *)
+
+let rec a n =
+  if n = 0 then 3
+  else 2 * (a (n - 1)) - 1
+
+(* テスト *)
+let test1 = a 0 = 3
+let test2 = a 1 = 5
+let test3 = a 2 = 9
+let test4 = a 3 = 17
